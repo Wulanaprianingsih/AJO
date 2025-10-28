99192175ff1114d2bc2c045a54ab3327e57a3f4d
Wulanaprianingsih <wulaapriiii@gmail.com>
2025-10-28 08:00:17 +0700

    fix ConfirmEmailPage

--- src/app/auth/confirm-email/page.tsx
+++ src/app/auth/confirm-email/page.tsx
@@ -3,9 +3,17 @@
 import ConfirmEmail from "modules/auth/components/ConfirmEmail";
 import { useSearchParams } from "next/navigation";
 
-export default function Page() {
+export default function ConfirmEmailPage() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email") || "";
+
+  if (!email) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p>Email tidak ditemukan. Silakan kembali ke halaman sebelumnya.</p>
+      </div>
+    );
+  }
 
   return <ConfirmEmail email={email} />;
 }
