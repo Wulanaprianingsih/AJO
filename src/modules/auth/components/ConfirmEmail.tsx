f94a0ee740dcdd5500ccc3a9f2ae4fd211e219b9
Wulanaprianingsih <wulaapriiii@gmail.com>
2025-10-28 08:17:15 +0700

    fix: remove button open email

--- src/modules/auth/components/ConfirmEmail.tsx
+++ src/modules/auth/components/ConfirmEmail.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Card, Typography, Space, Button } from "antd";
+
+import { Card, Typography, Space } from "antd";
 import { MailOutlined } from "@ant-design/icons";
 
 const { Title, Paragraph } = Typography;
@@ -9,30 +10,6 @@ interface ConfirmEmailProps {
 }
 
 export default function ConfirmEmail({ email }: ConfirmEmailProps) {
-  const getEmailLink = () => {
-    const domain = email.split("@")[1]?.toLowerCase();
-    const userAgent = navigator.userAgent || navigator.vendor;
-
-    const isMobile = /android|iphone|ipad|ipod/i.test(userAgent);
-
-    if (isMobile) {
-      if (domain === "gmail.com") return "googlegmail://";
-      if (domain === "outlook.com" || domain === "hotmail.com")
-        return "ms-outlook://";
-      return `https://mail.google.com/mail/u/0/#inbox`;
-    } else {
-      if (domain === "gmail.com") return "https://mail.google.com";
-      if (domain === "yahoo.com") return "https://mail.yahoo.com";
-      if (domain === "outlook.com" || domain === "hotmail.com")
-        return "https://outlook.live.com";
-      return "https://www.google.com";
-    }
-  };
-
-  const handleOpenEmail = () => {
-    window.open(getEmailLink(), "_blank");
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-yellow-50 px-4">
       <Card
@@ -58,9 +35,6 @@ export default function ConfirmEmail({ email }: ConfirmEmailProps) {
           <Paragraph type="secondary" style={{ fontSize: 12 }}>
             Jika tidak menerima email, cek folder spam.
           </Paragraph>
-          <Button type="primary" block onClick={handleOpenEmail}>
-            Buka Email
-          </Button>
         </Space>
       </Card>
     </div>
