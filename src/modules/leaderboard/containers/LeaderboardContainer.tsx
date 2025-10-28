2881c618c50db5343dc9aa182740a1e3c3f602ce
Wulanaprianingsih <wulaapriiii@gmail.com>
2025-10-28 12:04:33 +0700

    change leaderboard logic

--- src/modules/leaderboard/containers/LeaderboardContainer.tsx
+++ src/modules/leaderboard/containers/LeaderboardContainer.tsx
@@ -4,20 +4,18 @@ import LeaderboardComponent from "../components/LeaderboardComponent";
 import { fetchAllUser } from "services/userService";
 
 export default function LeaderboardContainer() {
-  const today = new Date();
-  const dayOfWeek = today.getDay();
-  const fetchMondayBoard = async () => {
-    const hours = today.getHours();
-    const minutes = today.getMinutes();
-    if (dayOfWeek === 1 && hours === 0 && minutes === 0) {
-      fetchAllUser();
-    }
-  };
+  // const today = new Date();
+  // const dayOfWeek = today.getDay();
+  // const fetchMondayBoard = async () => {
+  //   const hours = today.getHours();
+  // const minutes = today.getMinutes();
+  // if (dayOfWeek === 3 && hours === 0 && minutes === 0) {
+  // }
+  // };
 
   useEffect(() => {
-    fetchMondayBoard();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [today]);
+    fetchAllUser();
+  }, []);
 
   return <LeaderboardComponent />;
 }
